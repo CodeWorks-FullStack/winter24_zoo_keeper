913aac4145bf269134b22c5073711fc4d5e3b5fa
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-01 11:13:11 -0700

    drawing animal stats

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const animals = [
     name: 'tiger',
     emoji: '🐅',
     mood: '😀',
-    hunger: 50
+    hunger: 70
   },
   {
     name: 'larry',
     emoji: '🦆',
     mood: '😀',
-    hunger: 50
+    hunger: 30
   },
   {
     name: 'oslo',
     emoji: '🦧',
-    mood: '😀',
-    hunger: 50
+    mood: '😐',
+    hunger: 14
   },
 ]
 
@@ -32,5 +32,52 @@ function feedLarry() {
 function feedAnimal(animalName) {
   const foundAnimal = animals.find(animal => animal.name == animalName)
   foundAnimal.hunger++
+
+  // NOTE CLAMP value
+  if (foundAnimal.hunger > 100) {
+    foundAnimal.hunger = 100
+  }
+
   console.log(`My name is ${foundAnimal.name} and my hunger is ${foundAnimal.hunger}`);
+  drawTigerStats()
 }
+
+function drawTigerStats() {
+  const tiger = animals.find(animal => animal.name == 'tiger')
+
+  const tigerElement = document.getElementById('tiger')
+  console.log(tigerElement);
+  // NOTE looks through the tiger element (col-4) for an element with the class of stats
+  const statsElement = tigerElement.querySelector('.stats')
+
+  // @ts-ignore
+  statsElement.innerText = `${tiger.name} | ${tiger.mood} | Hunger: ${tiger.hunger}%`
+}
+
+function drawLarryStats() {
+  const larry = animals.find(animal => animal.name == 'larry')
+
+  const larryElement = document.getElementById('larry')
+
+  const statsElement = larryElement.querySelector('.stats')
+
+  console.log(statsElement);
+
+  // @ts-ignore
+  statsElement.innerText = `${larry.name} | ${larry.mood} | Hunger: ${larry.hunger}%`
+}
+
+function drawAnimalStats() {
+  animals.forEach(animal => {
+    const animalElement = document.getElementById(animal.name)
+    console.log(animalElement);
+
+    const statsElement = animalElement.querySelector('.stats')
+    console.log(statsElement);
+
+    // @ts-ignore
+    statsElement.innerText = `${animal.name} | ${animal.mood} | Hunger: ${animal.hunger}%`
+  })
+}
+
+drawTigerStats()
